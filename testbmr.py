cccddd71299b434a8ebff7c47047e5183cbc0d7a
wscheib2000 <wscheib@hotmail.com>
2021-04-01 18:22:59 -0400

    Delete test files

--- testbmr.py
+++
@@ -1,7 +0,0 @@
-import bmr
-
-print(bmr.st_jeor(74.7, 162.9, 19, 'female'))
-print(bmr.harris_benedict(74.7, 162.9, 19, 'female'))
-print(bmr.revised_harris_benedict(74.7, 162.9, 19, 'female'))
-print(bmr.katch_mcardle(74.7, 30))
-print(bmr.katch_mcardle(74.7, .3))
--- testconversion.py
+++
@@ -1,6 +0,0 @@
-import conversion
-
-print(conversion.c2f(31))
-print(conversion.c2f(100))
-print(conversion.f2c(-40))
-print(conversion.f2c(72))
